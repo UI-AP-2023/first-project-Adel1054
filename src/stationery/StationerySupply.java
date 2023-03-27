8d432711296f9ace091bdcce9f80b4ac0b61a3b0
Adel Pournaderi <adelp1054@gmail.com>
2023-03-27 09:59:07 +0430

    Added StationerySupply class.

---
+++ src/stationery/StationerySupply.java
@@ -0,0 +1,12 @@
+package stationery;
+
+public class StationerySupply {
+    private final String country;
+    StationerySupply(String country){
+        this.country=country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
