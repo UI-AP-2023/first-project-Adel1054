4c5dff08cda28951d4515204fb46d9436effa978
Adel Pournaderi <adelp1054@gmail.com>
2023-03-27 09:56:57 +0430

    Adding PC class.

---
+++ src/digitalCommodities/PC.java
@@ -0,0 +1,22 @@
+package digitalCommodities;
+
+import category.Category;
+
+public class PC extends DigitalCommodity{
+    private final int ramCapacity;
+    private final String cpuType;
+    PC(String ID, String name, double price, int availableCount, Category category, double weight, String dimensions,int ramCapacity,String cpuType){
+        super(ID,name,price,availableCount,category,weight,dimensions);
+        this.cpuType=cpuType;
+        this.ramCapacity=ramCapacity;
+    }
+
+    public int getRamCapacity() {
+        return ramCapacity;
+    }
+
+    public String getCpuType() {
+        return cpuType;
+    }
+    
+}
