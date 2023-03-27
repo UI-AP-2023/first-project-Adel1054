e76685229588a866cb1c63c77f0c23d2ebe14f4c
Adel Pournaderi <adelp1054@gmail.com>
2023-03-27 12:42:38 +0430

    Modified Comment class.

--- src/comment/Comment.java
+++ src/comment/Comment.java
@@ -1,7 +1,9 @@
 package comment;
 
+import consumer.Consumer;
+
 public class Comment {
-    //user
+    private final Consumer consumer;
     private final String commodityID;
     private String text;
     enum Status{
@@ -9,15 +11,36 @@ public class Comment {
     }
     private Status status;
     boolean userHasBought;
-    Comment(String commodityID,String text,boolean userHasBought){
+    Comment(String commodityID,String text,boolean userHasBought,Consumer consumer){
+        this.consumer=consumer;
         this.commodityID=commodityID;
         this.text=text;
         status=Status.TOBECONSIDERED;
         this.userHasBought=userHasBought;
     }
 
+    public Consumer getConsumer() {
+        return consumer;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getCommodityID() {
+        return commodityID;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
     @Override
     public String toString() {
-        return super.toString();
+        return consumer.getUsername()+" about commodity "+commodityID+"\n"+"    "+text;
     }
 }
