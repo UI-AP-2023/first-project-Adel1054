e1ec1a402dd04c14ffd37118bbebd04e523d2d8d
Adel Pournaderi <adelp1054@gmail.com>
2023-03-28 17:29:06 +0330

    Modified CommodityController and Commodity classes.

--- src/controller/CommodityController.java
+++ src/controller/CommodityController.java
@@ -17,37 +17,87 @@ import java.util.ArrayList;
 public class CommodityController {
     private final ArrayList<Commodity> commodities;
 
-    private void addComestible(String name, double price, int availableCount, String productionDate, String expirationDate){
-        commodities.add(new Comestible(name,price,availableCount,productionDate,expirationDate));
+    private void addComestible(String name, double price, int availableCount, String productionDate, String expirationDate) {
+        commodities.add(new Comestible(name, price, availableCount, productionDate, expirationDate));
+        Commodity.addToComestibleCount();
     }
-    private void addFlashMemory(String name, double price, int availableCount,double weight, String dimensions, double capacity, double usbVersion){
-        commodities.add(new FlashMemory(name,price,availableCount,weight,dimensions,capacity,usbVersion));
+
+    private void addFlashMemory(String name, double price, int availableCount, double weight, String dimensions, double capacity, double usbVersion) {
+        commodities.add(new FlashMemory(name, price, availableCount, weight, dimensions, capacity, usbVersion));
+        Commodity.addToDigitalCount();
     }
-    private void addPC(String name, double price, int availableCount, double weight, String dimensions, int ramCapacity, String cpuType){
-        commodities.add(new PC(name,price,availableCount,weight,dimensions,ramCapacity,cpuType));
+
+    private void addPC(String name, double price, int availableCount, double weight, String dimensions, int ramCapacity, String cpuType) {
+        commodities.add(new PC(name, price, availableCount, weight, dimensions, ramCapacity, cpuType));
+        Commodity.addToDigitalCount();
     }
-    private void addSSD(String name, double price, int availableCount, double weight, String dimensions, double capacity, double readSpeed, double writeSpeed){
-        commodities.add(new SSD(name,price,availableCount,weight,dimensions,capacity,readSpeed,writeSpeed));
+
+    private void addSSD(String name, double price, int availableCount, double weight, String dimensions, double capacity, double readSpeed, double writeSpeed) {
+        commodities.add(new SSD(name, price, availableCount, weight, dimensions, capacity, readSpeed, writeSpeed));
+        Commodity.addToDigitalCount();
     }
-    private void addNotebook(String name, double price, int availableCount, String country, int numberOfPages, String pageType){
-        commodities.add(new Notebook(name,price,availableCount,country,numberOfPages,pageType));
+
+    private void addNotebook(String name, double price, int availableCount, String country, int numberOfPages, String pageType) {
+        commodities.add(new Notebook(name, price, availableCount, country, numberOfPages, pageType));
+        Commodity.addToStationeryCount();
     }
-    private void addPen(String name, double price, int availableCount, String country, String colour){
-        commodities.add(new Pen(name,price,availableCount,country,colour));
+
+    private void addPen(String name, double price, int availableCount, String country, String colour) {
+        commodities.add(new Pen(name, price, availableCount, country, colour));
+        Commodity.addToStationeryCount();
     }
-    private void addPencil(String name, double price, int availableCount, String country, Pencil.PencilType pencilType){
-        commodities.add(new Pencil(name,price,availableCount,country,pencilType));
+
+    private void addPencil(String name, double price, int availableCount, String country, Pencil.PencilType pencilType) {
+        commodities.add(new Pencil(name, price, availableCount, country, pencilType));
+        Commodity.addToStationeryCount();
     }
-    private void addAutomobile(String name, double price, int availableCount, String companyName, boolean isAutomatic, int motorVolume){
-        commodities.add(new Automobile(name,price,availableCount,companyName,isAutomatic,motorVolume));
+
+    private void addAutomobile(String name, double price, int availableCount, String companyName, boolean isAutomatic, int motorVolume) {
+        commodities.add(new Automobile(name, price, availableCount, companyName, isAutomatic, motorVolume));
+        Commodity.addToVehicleCount();
     }
-    private void addBicycle(String name, double price, int availableCount, String companyName, Bicycle.BicycleType bicycleType){
-        commodities.add(new Bicycle(name,price,availableCount,companyName,bicycleType));
+
+    private void addBicycle(String name, double price, int availableCount, String companyName, Bicycle.BicycleType bicycleType) {
+        commodities.add(new Bicycle(name, price, availableCount, companyName, bicycleType));
+        Commodity.addToVehicleCount();
     }
+
     private void removeCommodity(Commodity commodity) {
         commodities.remove(commodity);
     }
-
+    private void changeCommodityName(String ID,String newName){
+        int commodityIndex=-1;
+        for(Commodity commodity:commodities){
+            if(commodity.getID().equals(ID)){
+                commodityIndex=commodities.indexOf(commodity);
+            }
+        }
+        if(commodityIndex!=-1){
+            commodities.get(commodityIndex).setName(newName);
+        }
+    }
+    private void changeCommodityPrice(String ID,double newPrice){
+        int commodityIndex=-1;
+        for(Commodity commodity:commodities){
+            if(commodity.getID().equals(ID)){
+                commodityIndex=commodities.indexOf(commodity);
+            }
+        }
+        if(commodityIndex!=-1){
+            commodities.get(commodityIndex).setPrice(newPrice);
+        }
+    }
+    private void changeCommodityAvailableCount(String ID,int newAvailableCount){
+        int commodityIndex=-1;
+        for(Commodity commodity:commodities){
+            if(commodity.getID().equals(ID)){
+                commodityIndex=commodities.indexOf(commodity);
+            }
+        }
+        if(commodityIndex!=-1){
+            commodities.get(commodityIndex).setAvailableCount(newAvailableCount);
+        }
+    }
     CommodityController() {
         commodities = new ArrayList<>();
     }
--- src/model/commodity/Commodity.java
+++ src/model/commodity/Commodity.java
@@ -56,6 +56,14 @@ public abstract class Commodity {
         averageRating = sum / (ratings.size());
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public void changeAvailableCount(int change) {
         availableCount += change;
     }
@@ -64,27 +72,95 @@ public abstract class Commodity {
         return ratings;
     }
 
+    public int getAvailableCount() {
+        return availableCount;
+    }
+
+    public void setAvailableCount(int availableCount) {
+        this.availableCount = availableCount;
+    }
+
+    public static int getComestibleCount() {
+        return comestibleCount;
+    }
+
+    public static int getDigitalCount() {
+        return digitalCount;
+    }
+
+    public static int getStationeryCount() {
+        return stationeryCount;
+    }
+
+    public static int getVehicleCount() {
+        return vehicleCount;
+    }
+
     private String IDBuilder() {
         StringBuilder ID = new StringBuilder();
         ID.append(category.toString(), 0, 4);
         ID.append(name, 0, 4);
         switch (category) {
             case COMESTIBLE:
+                switch (String.valueOf(comestibleCount).length()){
+                    case 1: ID.append("000");break;
+                    case 2: ID.append("00");break;
+                    case 3: ID.append("0");break;
+                }
                 ID.append(comestibleCount++);
                 break;
             case DIGITAL:
+                switch (String.valueOf(digitalCount).length()){
+                    case 1: ID.append("000");break;
+                    case 2: ID.append("00");break;
+                    case 3: ID.append("0");break;
+                }
                 ID.append(digitalCount++);
                 break;
             case STATIONERY:
+                switch (String.valueOf(stationeryCount).length()){
+                    case 1: ID.append("000");break;
+                    case 2: ID.append("00");break;
+                    case 3: ID.append("0");break;
+                }
                 ID.append(stationeryCount++);
                 break;
             case VEHICLE:
+                switch (String.valueOf(vehicleCount).length()){
+                    case 1: ID.append("000");break;
+                    case 2: ID.append("00");break;
+                    case 3: ID.append("0");break;
+                }
                 ID.append(vehicleCount++);
                 break;
         }
         return ID.toString();
     }
 
+    public static void addToDigitalCount() {
+        Commodity.digitalCount++;
+    }
+    public static void decreaseDigitalCount() {
+        Commodity.digitalCount--;
+    }
+    public static void addToComestibleCount(){
+        comestibleCount++;
+    }
+    public static void decreaseComestibleCount(){
+        comestibleCount--;
+    }
+    public static void addToStationeryCount() {
+        Commodity.stationeryCount++;
+    }
+    public static void decreaseStationeryCount() {
+        Commodity.stationeryCount--;
+    }
+    public static void addToVehicleCount() {
+        Commodity.vehicleCount++;
+    }
+    public static void decreaseVehicleCount() {
+        Commodity.vehicleCount--;
+    }
     @Override
     public String toString() {
         return "Name: " + name + " ID: " + ID + " Category: " + category + " Average rating: " + averageRating + " Price: " + price + "$";
