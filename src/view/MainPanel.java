11054814265bd671f33808514296cef8f3203b18
Adel Pournaderi <adelp1054@gmail.com>
2023-03-30 20:04:49 +0330

    Added view classes for admin consumer and main page.

---
+++ src/view/MainPanel.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.util.Scanner;
+
+public class MainPanel {
+    private Scanner input;
+    public MainPanel(){
+        input=new Scanner(System.in);
+    }
+    public void mainPage(){
+        int command= input.nextInt();
+        switch (command){
+            case 1:
+            case 2:
+        }
+    }
+
+}
